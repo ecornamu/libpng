6e56276683388feb5c1dfd2341c8452064544bf7
Yann <yann.savioz@gmail.com>
2025-05-15 21:57:31 +0200

    Finished read_fuzzer

--- contrib/oss-fuzz/libpng_read_fuzzer.cc
+++ contrib/oss-fuzz/libpng_read_fuzzer.cc
@@ -21,8 +21,6 @@
 
 #define PNG_INTERNAL
 #include "png.h"
-#include "pnglibconf.h"
-#include "pngconf.h"
 
 #define PNG_CLEANUP \
   if(png_handler.png_ptr) \
@@ -97,6 +95,7 @@ void default_free(png_structp, png_voidp ptr) {
 static const int kPngHeaderSize = 8;
 
 void check_metadata(const PngObjectHandler &png_handler) {
+
   png_uint_32 width = png_get_image_width(png_handler.png_ptr, png_handler.info_ptr);
   png_uint_32 height = png_get_image_height(png_handler.png_ptr, png_handler.info_ptr);
 
@@ -109,12 +108,13 @@ void check_metadata(const PngObjectHandler &png_handler) {
   int interlace_type = png_get_interlace_type(png_handler.png_ptr, png_handler.info_ptr);
 
 
+
   // --- METADATA RETRIEVAL ---
   png_get_IHDR(png_handler.png_ptr, png_handler.info_ptr, &width,
                     &height, &bit_depth, &color_type, &interlace_type,
                     &compression_type, &filter_type);
 
-  png_uint_32 valid_t = png_get_valid(png_handler.png_ptr, png_handler.info_ptr, ~0U);
+  //png_uint_32 valid_t = png_get_valid(png_handler.png_ptr, png_handler.info_ptr, ~0U);
   png_uint_32 uint1, uint2;
   png_bytep bytep_1, bytep_2, bytep_3;
   png_color_16p color1, color2, color3;
@@ -146,8 +146,8 @@ void check_metadata(const PngObjectHandler &png_handler) {
 
   // Retrieve pHYs chunk (resolution)
   if (PNG_INFO_pHYs) {
-    png_uint_32 res_x = uint1;
-    png_uint_32 res_y = uint2;
+    png_uint_32 res_x;
+    png_uint_32 res_y;
     int unit_type = int1;
     png_get_pHYs(png_handler.png_ptr, png_handler.info_ptr,
                  &res_x, &res_y, &unit_type);
@@ -173,6 +173,7 @@ void check_metadata(const PngObjectHandler &png_handler) {
   // Retrieve cHRM chunk (chromaticity)
 #ifdef PNG_cHRM_SUPPORTED
   if (PNG_INFO_cHRM) {
+
 
     png_get_cHRM(png_handler.png_ptr, png_handler.info_ptr,
                  &white_x, &white_y, &red_x, &red_y,
@@ -395,13 +396,13 @@ if (PNG_INFO_bKGD) {
   #endif
 }
 
-
 // Entry point for LibFuzzer.
 // Roughly follows the libpng book example:
 // http://www.libpng.org/pub/png/book/chapter13.html
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   if (size < kPngHeaderSize) {
     return 0;
+
   }
 
   if (size > 100000) return 0;  // Skip overly large files
@@ -421,7 +422,6 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 
   png_handler.png_ptr = png_create_read_struct
     (PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
-
   if (!png_handler.png_ptr) {
     return 0;
   }
@@ -467,14 +467,30 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     return 0;
   }
 
-  png_uint_32 width, height;
-  int filter_type;
+
+  png_uint_32 width = png_get_image_width(png_handler.png_ptr, png_handler.info_ptr);
+  png_uint_32 height = png_get_image_height(png_handler.png_ptr, png_handler.info_ptr);
 
   // Before allocations:
   const size_t kMaxAlloc = 100*1024*1024; // 100MB
   if (width > 10000 || height > 10000 || width*height > kMaxAlloc/4) {
     return 0;
   }
+
+  int bit_depth = png_get_bit_depth(png_handler.png_ptr, png_handler.info_ptr);
+  int color_type = png_get_color_type(png_handler.png_ptr, png_handler.info_ptr);
+  int compression_type = png_get_compression_type(png_handler.png_ptr, png_handler.info_ptr);
+  int filter_type = png_get_filter_type(png_handler.png_ptr, png_handler.info_ptr);
+  int interlace_type = png_get_interlace_type(png_handler.png_ptr, png_handler.info_ptr);
+
+
+
+  if (!png_get_IHDR(png_handler.png_ptr, png_handler.info_ptr, &width,
+                      &height, &bit_depth, &color_type, &interlace_type,
+                      &compression_type, &filter_type)) {
+    PNG_CLEANUP
+    return 0;
+                      }
 
   if (rand() % 7 == 0) {
     check_metadata(png_handler);
